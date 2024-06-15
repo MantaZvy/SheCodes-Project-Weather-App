b044a956f030febcd788ba364d1a4cefa6dd6572
MantaZvy <mantaszvy@gmail.com>
2024-06-15 18:56:19 +0100

    changed js syntax for local time.

--- src/weatherApp_JS.js
+++ src/weatherApp_JS.js
@@ -4,17 +4,51 @@ function displayTemperature(response) {
   console.log(response);
   let tempElement = document.querySelector("#temp-value");
   tempElement.innerHTML = Math.round(response.data.temperature.current);
+
   let displayCity = document.querySelector("#displayCity");
   displayCity.innerHTML = response.data.city;
+
   let displayHumidity = document.querySelector("#humidity");
   let humidity = response.data.temperature.humidity;
   displayHumidity.innerHTML = `${humidity}%`;
+
   let displayWind = document.querySelector("#wind");
   let wind = response.data.wind.speed;
   displayWind.innerHTML = `${wind} p/h`;
+
   let weatherInfo = document.querySelector("#weather-description");
   let weatherDescription = response.data.condition.description;
   weatherInfo.innerHTML = `${weatherDescription}`;
+
+  let timeElement = document.querySelector("#time");
+  let timeStamp = response.data.time * 1000;
+  let date = new Date(timeStamp);
+  timeElement.innerHTML = formatDate(date);
+}
+//Feature- Live Time
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 //API Key
 //Function to handle search
@@ -27,36 +61,6 @@ function search(event) {
 
   axios.get(apiUrl).then(displayTemperature);
 }
-//Feature 1 - Live Time
-function formatDate(now) {
-  let hour = now.getHours();
-  let minutes = now.getMinutes();
-
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-  if (hour < 10) {
-    hour = `0${hour}`;
-  }
-
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let todaysDay = days[now.getDay()];
-
-  return `${todaysDay} ${hour}:${minutes}`;
-}
 
 let searchingBtn = document.querySelector("#search-bar");
 searchingBtn.addEventListener("submit", search);
-
-let spanElement = document.querySelector("#dayNow");
-let now = new Date();
-
-spanElement.innerHTML = formatDate(now);
